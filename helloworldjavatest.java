cdc380316ec29b47aef176159ab8603c217769d9
root <root@ip-172-31-29-159.ec2.internal>
2024-11-04 09:29:45 +0000

    made once more change to the print command

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Change has been made!!!");
+		System.out.println("Change is essential for life!!!");
 	}
 }
