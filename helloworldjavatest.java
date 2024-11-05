ea91b3615e849fa5ffc3d16ef35614959bab22ad
root <root@ip-172-31-29-159.ec2.internal>
2024-11-05 02:08:11 +0000

    made changes to the print line

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Change is essential both mentally and physicall!!!");
+		System.out.println("Change is good in life!!!");
 	}
 }
